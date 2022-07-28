71aba06036d56e153583ba7fe467bc4a8cab1ff9
bin <binzzz1997@gmail.com>
2022-07-28 14:16:27 +0800

    修改rtp 传输策略

--- live/liveMedia/RTPInterface.cpp
+++ live/liveMedia/RTPInterface.cpp
@@ -357,7 +357,7 @@ Boolean RTPInterface::sendRTPorRTCPPacketOverTCP(u_int8_t* packet, unsigned pack
     framingHeader[1] = streamChannelId;
     framingHeader[2] = (u_int8_t) ((packetSize&0xFF00)>>8);
     framingHeader[3] = (u_int8_t) (packetSize&0xFF);
-    if (!sendDataOverTCP(socketNum, tlsState, framingHeader, 4, False)) break;
+    if (!sendDataOverTCP(socketNum, tlsState, framingHeader, 4, True)) break;
 
     if (!sendDataOverTCP(socketNum, tlsState, packet, packetSize, True)) break;
 #ifdef DEBUG_SEND
@@ -374,7 +374,7 @@ Boolean RTPInterface::sendRTPorRTCPPacketOverTCP(u_int8_t* packet, unsigned pack
 }
 
 #ifndef RTPINTERFACE_BLOCKING_WRITE_TIMEOUT_MS
-#define RTPINTERFACE_BLOCKING_WRITE_TIMEOUT_MS 500
+#define RTPINTERFACE_BLOCKING_WRITE_TIMEOUT_MS 5000
 #endif
 
 Boolean RTPInterface::sendDataOverTCP(int socketNum, TLSState* tlsState,
